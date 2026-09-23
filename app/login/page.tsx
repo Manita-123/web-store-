2e97b81264523e24f23b291bdba0eab1d963a180
manita <nityaaarohi175@gmail.com>
2026-09-23 22:50:43 +0530

    intial commit

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoginPage() {
+  return (
+    <div className='my-5 '>
+      <h1 className='text-center text-2xl '>Login</h1>
+    </div>
+  )
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function SignupPage() {
+  return (
+    <div>
+      <h1 className="text-2xl">Signup</h1>
+    </div>
+  )
+}
